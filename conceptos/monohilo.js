34033f6606e14369efaafaf147bd8aaa70d064e1
Fabian Salgado <Fabian Salgado>
2020-08-01 19:53:32 -0400

    clases 4,5,6 conceptos básicps

---
+++ conceptos/monohilo.js
@@ -0,0 +1,12 @@
+console.log('Hola Mundo');
+let i = 0;
+setInterval(() => {
+	console.log(i);
+	i++;
+	// if (i === 5) {
+    //     console.log('forzamos error')
+	// 	var a = 3 + z;
+	// }
+}, 1000);
+
+console.log('Segunda instrucción')
